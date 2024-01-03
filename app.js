1d45d93a2019a82b6ef029498699339df7c9ec6a
Ambika Prasad <akashboard2021@gmail.com>
2024-01-03 14:40:23 +0530

    allow any website

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://bcccafe.vercel.app"],
+    // origin: ["http://localhost:5173", "https://bcccafe.vercel.app"],
+    origin: "*",
   })
 );
 app.use(express.json());
