8d621d44a978416e9a956b28c669031c1130154d
Ambika Prasad <akashboard2021@gmail.com>
2024-07-15 17:02:19 +0530

    spell correction

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Athorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
