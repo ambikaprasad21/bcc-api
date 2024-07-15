f1b17c0cb50ffdb5e6483c2692a166486309b513
Ambika Prasad <akashboard2021@gmail.com>
2024-07-15 16:39:20 +0530

    removed wildcard origin *

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Athorization"
